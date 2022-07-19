727a1b1e1f3f6fb612d9e11f954168bae8468de0
Martin Øvsttun <marovsttun@gmail.com>
2022-07-19 13:18:23 +0200

    Renamed variables to fit naming convention

--- Assets/Scripts/CardZoom.cs
+++ Assets/Scripts/CardZoom.cs
@@ -1,37 +1,34 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CardZoom : MonoBehaviour
 {
 
-    public GameObject Canvas;
-    private GameObject zoomCard;
+    public GameObject canvas;
+    private GameObject _zoomCard;
 
     private void Awake()
     {
-        Canvas = GameObject.Find("MainCanvas");
+        canvas = GameObject.Find("MainCanvas");
     }
 
     public void OnClickEnter()
     {
         Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        zoomCard = Instantiate(gameObject, Canvas.transform, false);
-        zoomCard.transform.position = new Vector3(worldPoint.x, worldPoint.y + 2f, 0);
-        zoomCard.transform.localScale = new Vector3(2f, 2f, 1f);
-        zoomCard.layer = LayerMask.NameToLayer("Zoom");
+        _zoomCard = Instantiate(gameObject, canvas.transform, false);
+        _zoomCard.transform.position = new Vector3(worldPoint.x, worldPoint.y + 2f, 0);
+        _zoomCard.transform.localScale = new Vector3(2f, 2f, 1f);
+        _zoomCard.layer = LayerMask.NameToLayer("Zoom");
         
         RectTransform cardRectTransform = gameObject.GetComponent<RectTransform>();
-        RectTransform rectTransform = zoomCard.GetComponent<RectTransform>();
+        RectTransform rectTransform = _zoomCard.GetComponent<RectTransform>();
         Rect rect = cardRectTransform.rect;
         rectTransform.sizeDelta = new Vector2(rect.width, rect.height);
     }
 
     public void OnClickExit()
     {
-        Destroy(zoomCard);
+        Destroy(_zoomCard);
     }
 
 
