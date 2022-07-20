a1751e128a30a51acc09e0de5eb1d864ee820548
Martin Øvsttun <marovsttun@gmail.com>
2022-07-20 14:18:53 +0200

    Implemented logic for disallowing dragging of placed cards and cards when not player's turn

--- Assets/Scripts/DragCards.cs
+++ Assets/Scripts/DragCards.cs
@@ -1,14 +1,14 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DragCards : MonoBehaviour
 {
 
     public GameObject canvas;
-    private bool _isDragging = false;
-    private bool _isDropable = false;
+    private bool _isDragging;
+    private bool _isDroppable;
+    private bool _isDropped;
+    private bool _isNotAllowedDragging;
     private GameObject _dropZone;
     private GameObject _startParent;
     private Vector3 _startPosition;
@@ -22,7 +22,7 @@ public class DragCards : MonoBehaviour
     void Update()
     {
         if (_isDragging)
-        { // TODO: if player's turn
+        {
             Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector3(worldPoint.x, worldPoint.y, 0);
             transform.SetParent(canvas.transform, true);
@@ -31,35 +31,55 @@ public class DragCards : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        _isDropable = true;
+        _isDroppable = true;
         _dropZone = col.gameObject;
     }
 
     private void OnCollisionExit2D(Collision2D other)
     {
-        _isDropable = false;
+        _isDroppable = false;
         _dropZone = null;
     }
 
     public void BeginDrag() // TODO: Disallow pickup of played cards
     {
+        _isNotAllowedDragging = DragNotAllowed();
+        if (_isNotAllowedDragging) return;
+
         _startParent = transform.parent.gameObject;
         _startPosition = transform.position;
         _isDragging = true;
     }
 
+    private bool DragNotAllowed()
+    {
+        if (_isDropped) return true;
+        if (!GameModel.Instance.PlayerTurn) return true;
+        return false;
+    }
+
     public void EndDrag()
     {
+        if (_isNotAllowedDragging) return;
+        
         _isDragging = false;
-        if (_isDropable)
+        if (_isDroppable)
         {
-            transform.SetParent(_dropZone.transform, false);
-            GameModel.Instance.SetOpponentTurn();
+            DropCard();
         }
         else
         {
             transform.position = _startPosition;
             transform.SetParent(_startParent.transform, false);
         }
+    }
+
+    private void DropCard()
+    {
+        transform.SetParent(_dropZone.transform, false);
+        GameModel.Instance.PlayerTurn = false;
+        bool wasRemoved = GameModel.Instance.RemovePlayedCard(gameObject);
+        if (!wasRemoved) throw new Exception("Card not removed from internal representation");
+        _isDropped = true;
     }
 }
