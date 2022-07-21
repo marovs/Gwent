354c3186819b90cfdcaead4b99874a9cb5df045d
Martin Øvsttun <marovsttun@gmail.com>
2022-07-21 15:00:56 +0200

    Added attribute power to cards and code to change text displaying card power based on internal representation

--- Assets/Scripts/CardAttributes.cs
+++ Assets/Scripts/CardAttributes.cs
@@ -1,17 +1,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CardAttributes : MonoBehaviour
 {
-
     public byte id;
     public string lane;
+    public int power;
+
+    private TextMeshProUGUI _powerText;
     
     private void Awake()
     {
         if (lane != null) gameObject.layer = LayerMask.NameToLayer(lane);
+
+        TextMeshProUGUI[] textComponents = GetComponentsInChildren<TextMeshProUGUI>();
+        foreach (TextMeshProUGUI textComponent in textComponents)
+        {
+            if (textComponent.name.Equals("PowerText")) _powerText = textComponent;
+        }
+
+        try
+        {
+            _powerText.text = power.ToString();
+        }
+        catch (NullReferenceException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 
     // Update is called once per frame
